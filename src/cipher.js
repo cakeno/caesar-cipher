0b57ef4a875bd3f4c0c319b71c444c27b104ed5d
cakeno <camilla.noba@gmail.com>
2019-01-28 15:33:17 -0200

    primeiro cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,46 @@
 window.cipher = {
-  // ... 
+  encode: function() {
+    let string= document.getElementById('stringToEncode').value;
+    let offset= document.getElementById('offset').value;
+    let arrLetters= string.split('');
+    let arrToCode= [];
+    let stringCoded= [];
+
+// colocar a condição do alfabeto dentro do ASCII if...
+
+    for (i = 0; i < arrLetters.length; i++){
+
+      arrToCode.push((((arrLetters[i].charCodeAt(0) - 65 )+(parseInt(offset))) % 26) + 65);
+
+//não está funcionando direito
+//concat
+
+      for (x = i ; x < arrToCode.length; x++){
+          let stringCoded = String.fromCharCode(arrToCode);
+          //let stringCoded = String.fromCharCode(arrToCode.concat);
+
+console.log (stringCoded)
+            //console.log (document.getElementById('result').value = (stringCoded));
+          }
+          return document.getElementById('result').value = (stringCoded);
+        }
+      },
+
+  decode: function() {
+    let string= document.getElementById('stringToEncode').value;
+    let offset= document.getElementById('offset').value;
+    let arrLetters= string.split('');
+    let arrToCode= [];
+
+    for (i = 0; i < arrLetters.length; i++){
+
+      arrToCode.push(((arrLetters[i].charCodeAt(0) - 65 - offset) % 26) + 65);
+
+      for (x = 0; x < arrToCode.length; x++){
+          let stringCoded = String.fromCharCode(arrToCode[x]);
+              return document.getElementById('result').value = (stringCoded);
+            }
+          }
+        },
+
 };
