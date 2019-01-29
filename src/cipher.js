803d8bea1094a9e6796dc0c4d0ba7444a677c115
cakeno <camilla.noba@gmail.com>
2019-01-29 13:32:42 -0200

    cipher dois

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,9 @@
 window.cipher = {
+
+  //showResult: function (result) {
+  //document.getElementById('result').value = result;
+//},
+
   encode: function() {
     let string= document.getElementById('stringToEncode').value;
     let offset= document.getElementById('offset').value;
@@ -6,41 +11,45 @@ window.cipher = {
     let arrToCode= [];
     let stringCoded= [];
 
-// colocar a condição do alfabeto dentro do ASCII if...
-
     for (i = 0; i < arrLetters.length; i++){
-
-      arrToCode.push((((arrLetters[i].charCodeAt(0) - 65 )+(parseInt(offset))) % 26) + 65);
-
-//não está funcionando direito
-//concat
-
-      for (x = i ; x < arrToCode.length; x++){
-          let stringCoded = String.fromCharCode(arrToCode);
-          //let stringCoded = String.fromCharCode(arrToCode.concat);
-
-console.log (stringCoded)
-            //console.log (document.getElementById('result').value = (stringCoded));
-          }
-          return document.getElementById('result').value = (stringCoded);
-        }
-      },
+      let letterAsc= arrLetters[i].charCodeAt(0)
+      if(letterAsc >= 65 && letterAsc <= 90){
+        arrToCode.push((((letterAsc - 65) + parseInt(offset % 26) + 26) % 26) + 65);
+      } else if(letterAsc >= 97 && letterAsc <= 122){
+        arrToCode.push((((letterAsc - 97) + parseInt(offset % 26) + 26) % 26) + 97);
+      } else {
+        arrToCode.push(letterAsc);
+      };
+    for (x = i ; x < arrToCode.length; x++){
+        stringCoded.push (String.fromCharCode(arrToCode[x]));
+      };
+    };
+        console.log (result)
+      return document.getElementById('result').value = (stringCoded.join(""));
+        },
 
   decode: function() {
     let string= document.getElementById('stringToEncode').value;
     let offset= document.getElementById('offset').value;
     let arrLetters= string.split('');
     let arrToCode= [];
+    let stringCoded= [];
 
     for (i = 0; i < arrLetters.length; i++){
+      let letterAsc= arrLetters[i].charCodeAt(0)
+      if(letterAsc >= 65 && letterAsc <= 90){
+        arrToCode.push((((letterAsc - 65) - parseInt(offset % 26) + 26) % 26) + 65);
+      } else if(letterAsc >= 97 && letterAsc <= 122){
+        arrToCode.push((((letterAsc - 97) - parseInt(offset % 26) + 26) % 26) + 97);
+      } else {
+        arrToCode.push(letterAsc);
+      };
 
-      arrToCode.push(((arrLetters[i].charCodeAt(0) - 65 - offset) % 26) + 65);
-
-      for (x = 0; x < arrToCode.length; x++){
-          let stringCoded = String.fromCharCode(arrToCode[x]);
-              return document.getElementById('result').value = (stringCoded);
-            }
-          }
-        },
-
+      for (x = i ; x < arrToCode.length; x++){
+        stringCoded.push (String.fromCharCode(arrToCode[x]));
+      };
+    };
+    console.log (result)
+      return document.getElementById('result').value = (stringCoded.join(""));
+        }
 };
